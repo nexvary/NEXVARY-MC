56620838874d115d0ca5c5f8b42f358bea4d6a35
NEXVARY Inc <325503741+nexvary@users.noreply.github.com>
2026-09-23 12:23:59 -0700

    Import NEXVARY MC v1.3.0 resources

---
+++ app/src/main/java/com/nexvary/mc/AppLinks.java
@@ -0,0 +1,10 @@
+package com.nexvary.mc;
+
+public final class AppLinks {
+    private AppLinks() {}
+    public static final String WEBSITE = "https://nexvary.com/";
+    public static final String FACEBOOK = "https://www.facebook.com/share/14p9krEn5ij/";
+    public static final String EMAIL = "mailto:info@nexvary.com";
+    public static final String YOUTUBE = "https://www.youtube.com/@NexvaryInc";
+    public static final String X = "https://x.com/Nexvary";
+}
